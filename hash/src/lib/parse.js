97412d7b537250f694c804f122318d6f45de597d
koinotice <koinotice@gmail.com>
2019-02-19 01:14:51 +0800

    update account

--- hash/src/lib/parse.js
+++ hash/src/lib/parse.js
@@ -6,7 +6,7 @@ const Parse = require('parse/node');
 Parse.initialize("pmker","pmker", "Zheli123");
 //javascriptKey is required only if you have it on server.
 
-Parse.serverURL = 'http://localhost:7311/app/pmker'
+Parse.serverURL = 'http://0.0.0.0:7311/app/pmker'
 
 module.exports =   {
     Parse
